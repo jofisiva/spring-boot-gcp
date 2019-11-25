59537ce5cf341a792299325a18935dcd40819120
Sivabalan <sivabalan.murugesan@aimia.com>
2019-11-25 21:14:24 +0000

    Inital commit

---
+++ src/main/java/com/technxt/gcp/SpringBootGcpApplication.java
@@ -0,0 +1,24 @@
+package com.technxt.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/demo-gcp")
+@SpringBootApplication
+public class SpringBootGcpApplication {
+
+    @GetMapping
+    public String demo() {
+        return "Spring Boot Run in GCP";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGcpApplication.class, args);
+    }
+
+}
